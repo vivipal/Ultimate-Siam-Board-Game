9871dcf542cfa3d03cf2a49705d3a1dbeee347db
le seul bg de la team <gabriel.betton@gmail.com>
2021-03-16 15:12:32 +0100

    class Pion __init__

--- pion.py
+++ pion.py
@@ -5,7 +5,9 @@ class Pion():
     Classe décrivant les pions (Elephant, Rhino, Caillou)
     '''
 
-    def __init__(self, abcisse, ordonnee):
-            '''
-            Crée un pion aux coordonnées désirées
-            '''
+    def __init__(self, abscisse, ordonnee):
+        '''
+        Crée un pion aux coordonnées désirées
+        '''
+        self.coords = abscisse, ordonnee
+    
