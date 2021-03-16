34e48f2b6c010b1905ff5de1534c4a248d0631ba
le seul bg de la team <gabriel.betton@gmail.com>
2021-03-16 15:05:15 +0100

    class Pion

--- pion.py
+++ pion.py
@@ -1,1 +1,11 @@
 import numpy
+
+class Pion():
+    '''
+    Classe décrivant les pions (Elephant, Rhino, Caillou)
+    '''
+
+    def __init__(self, abcisse, ordonnee):
+            '''
+            Crée un pion aux coordonnées désirées
+            '''
