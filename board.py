297f0dacb09433d247633653c7f5fc1369d7603f
Virgile P <adtoviv@gmail.com>
2021-03-16 15:01:43 +0100

    Commit

--- board.py
+++ board.py
@@ -1,9 +1,14 @@
 
 
 class Board():
-    ''' Siam Board object
+    '''
 
+    Classe représentant le plateau de jeuet gérant le déroulement de la partie
 
     '''
     def __init__(self,size=(5,5)):
-        
+        """
+        size tuple represent the size of the board game.
+        """
+
+        self.width, self.height = size
