a0fbe0351394498ae27fae42e7eb41753c440a73
le seul bg de la team <gabriel.betton@gmail.com>
2021-04-09 16:53:33 +0200

    ajout __str__ et move()

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pion
 
 class Board(np.ndarray):
     '''
@@ -9,3 +10,32 @@ class Board(np.ndarray):
         """
         size tuple represent the size of the board game.
         """
+
+
+    def __str__(self):
+        """
+        affichage sipmifié du plateau de jjeu et
+        """
+        view = self[:]
+        view[type(view) == pion.Rhino] = "R"
+        view[type(view) == pion.Elephant] = "E"
+        view[type(view) == pion.Rocher] = "O"
+        view[view=None] = ""
+
+        return view
+
+    def move(animal, direction):
+        """
+        bouge un ou plusieurs animaux dans une direction spécifique si cela est possible
+        """
+        x = animal.x
+        y = animal.y
+
+        c_pour = 1
+        c_contre = 0
+        c_rocher = 0
+        i = 1
+        sens = -1 + (animal.direction=="E" or animal.direction=="S")*2   # valeur d'incrémentation, 1 si vers Nord ou Est, -1 si vers Sud ou Ouest
+
+        if direction=="N" or direction=="S":
+            while 0 <= y+sens*i <= 4 and self[x,y+sens*i]!=None:
