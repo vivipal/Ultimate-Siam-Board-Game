402ff09267a587c6d886954b9e53ffaa0808724f
Virgile P <adtoviv@gmail.com>
2021-06-04 12:57:40 +0200

    README updated

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 ### How to play ###
-You just need to run Siam.py with python3
+You just need to run Siam.py with python3 (numpy and PyQt5 are required)
 
+### Unittest ###
+You can run unit test on Board class and Rhino class by running test_board.py and test_rhino.py with python3
 
 #### TO DO ####
 
-- pb insert dont win
+- winning is not implemented yet by inserting a piece
+- add a background image on the UI
---
+++ test_rhino.py
@@ -0,0 +1,64 @@
+import unittest
+
+from pion import Animal, Rhino
+
+
+class TestRhino(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def testInit(self):
+        """
+        Test de la méthode d’initialisation
+        """
+        rhino = Rhino(2,3,180)
+
+        self.assertEqual(rhino.x,2)
+        self.assertEqual(rhino.y,3)
+        self.assertEqual(rhino.orientation,180)
+        self.assertEqual(rhino.espece,'R')
+
+    def testAnimal(self):
+        """
+        Test si un rhinocéros est bien un animal
+        """
+
+        r = Rhino(5, 1, 0)
+        self.assertIsInstance(r, Animal)
+
+
+    def testInitWrongPosition(self):
+        """
+        Test de l'initialisation d'une pièce avec des coordonnées négatives
+        """
+
+        with self.assertRaises(Exception):
+            a=Rhino(2,-2,0)
+            b=Rhino(-3,-1,0)
+            c=Rhino(-1,4,0)
+
+    def testInitWrongOrientation(self):
+        """
+        Test de l'initialisation d'une pièce avec une orientation impossible
+        """
+
+        with self.assertRaises(Exception):
+            a=Rhino(2,-2,80)
+
+
+
+    def testMove(self):
+        """
+        Test du mouvement d'une pièce
+        """
+
+        r = Rhino(1,2,90)
+        r.move(180)
+
+        self.assertEqual(r.x, 2)
+        self.assertEqual(r.y, 2)
+
+
+if __name__ == '__main__' :
+    unittest.main()
